bf2dc5b8610a6154bdbecef78bfb5d491f0f2bcb
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2024-02-23 03:51:56 +0530

    more work

--- src/components/UserMore.tsx
+++ src/components/UserMore.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import axios from 'axios'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
 
 const UserMore = () => {
+  // user id.....................
+  const [userId, setId] = useState('')
+  const [username, setname] = useState('')
+  useEffect(()=>{
+    const getUserDetails = async ()=>{
+      const res = await axios.get('/api/user/me');
+      const resname = await axios.get('/api/user/me');
+      setId(res.data.data._id);
+      setname(resname.data.data.username);
+    }
+    getUserDetails();
+  }, [])
+  
+  // Profile Api..............................
+  const [userPost, setUserPost] = useState([])
+  const [profilImg, setProfilImg] = useState([]);
+  const [dataBackground, setDataBackground] = useState([])
+  useEffect(()=>{
+    const fetchProfil = async ()=>{
+      try {
+        const resposts = await axios.get(`/api/post/usergetpost/${userId}`);
+        const resprofil = await axios.get(`/api/userget/getprofileImage/${userId}`)
+        const resbackground = await axios.get(`/api/userget/getprofilebackimage/${userId}`);
+        // console.log(response.data)
+        if (!resposts || !resprofil || !resbackground) {
+          throw new Error('Failed to fetch data');
+        }
+        setUserPost(resposts.data.postes)
+        setProfilImg(resprofil.data.profileimgs)
+        setDataBackground(resbackground.data.backimgs)
+      } catch (error) {
+        console.log(error, 'Error')
+      }
+    }
+    fetchProfil()
+  }, [userId])
+  
   return (
-    <div>
-      More
+    <div className='flex flex-wrap gap-3 p-2'>
+      {
+        userPost && userPost.map((exc: any)=>(
+          <div key={exc} className="">
+            <Image src={exc.imgposty} alt='Bird' className='bg-gray-200' width={70} height={70} />
+          </div>
+        ))
+      }
+      {
+        profilImg && profilImg.map((excv: any)=>(
+          <div key={excv} className="">
+            <Image src={excv.profilImg} alt='Bird' className='bg-gray-200' width={70} height={70} />
+          </div>
+        ))
+      }
+      {
+        dataBackground && dataBackground.map((excvr: any)=>(
+          <div key={excvr} className="">
+            <Image src={excvr.backImage} alt='Bird' className='bg-gray-200' width={70} height={70} />
+          </div>
+        ))
+      }
     </div>
   )
 }
